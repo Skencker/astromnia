037fd5b767e1ffc5f037e5ad1a9c5507fa7ce422
Skencker <Sandrinekencker@hotmail.com>
2022-06-24 17:16:07 +0200

    dark mode - menu

--- app.js
+++ app.js
@@ -9,17 +9,17 @@ let logo =document.querySelector('.logo');
 //console.log(button)
 //console.log(open_nav)
 
-button.addEventListener('click', () => {
+// button.addEventListener('click', () => {
 
-    bg.classList.toggle('opened')
-    position.classList.toggle('position')
-    ul.classList.toggle('opacity')
-    logo.classList.toggle('center')
+//     bg.classList.toggle('opened')
+//     position.classList.toggle('position')
+//     ul.classList.toggle('opacity')
+//     logo.classList.toggle('center')
     
-    for (let i = 0; i < lien.length; i++) {
-        lien[i].classList.toggle('opacity')
-    }
-})
+//     for (let i = 0; i < lien.length; i++) {
+//         lien[i].classList.toggle('opacity')
+//     }
+// })
 
 
 //particules 
